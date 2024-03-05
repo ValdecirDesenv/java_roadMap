f2bd0c27b8f4369d2a5186711cbf326c087e53b3
ValdecirDesenv <valdecir.neper@gmail.com>
2024-03-05 14:51:45 -0500

    "starting project"

---
+++ src/main/java/com/vdc/dev/roadmap/Roadmap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vdc.dev.roadmap;
+
+/**
+ *
+ * @author vdc
+ */
+public class Roadmap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
