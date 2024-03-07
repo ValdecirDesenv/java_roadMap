14d2411e8d44a98f040cfd2d881f0c1351f920ae
ValdecirDesenv <valdecir.neper@gmail.com>
2024-03-07 15:14:43 -0500

    "testing Enum and Serializations"

--- src/main/java/com/vdc/dev/roadmap/Roadmap.java
+++ src/main/java/com/vdc/dev/roadmap/Roadmap.java
@@ -4,6 +4,15 @@
 
 package com.vdc.dev.roadmap;
 
+import com.vdc.dev.code.Book;
+import com.vdc.dev.enums.Calendar;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author vdc
@@ -11,6 +20,72 @@ package com.vdc.dev.roadmap;
 public class Roadmap {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+  
+        callEnumSample();
+        callSerialization();
+        
+        
+        
+        
+        System.out.println("Hello Java best neil code!");
+    }
+    
+    
+    
+    
+    public static void callEnumSample(){
+         //==================== Enum ===========
+        Calendar month = Calendar.MARCH;
+        System.out.println(month.getName() + " is month number " + month.getValue());      
+        for (Calendar months: Calendar.values()){
+            System.out.println(" Enum value :" + months.name() +" n:" + months.getValue());
+        }
+        System.out.println("\n");
+    }
+    
+    
+    public static void callSerialization(){
+         //==================== Serialization ===========
+        Book bo = new Book("Sky Blue", 500);
+        bo.setValueForServerOnly(3800);
+
+        String filename = "resources//Book.obj";        
+        File fileNamePath = new File(filename).getAbsoluteFile();        
+        InputStream fileIn = Roadmap.class.getResourceAsStream(filename);        
+        FileOutputStream fileOut =null;
+        ObjectOutputStream objOut = null;
+        
+        //Serialization
+        try {
+            fileOut = new FileOutputStream(fileNamePath);
+            objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(bo);
+            fileOut.close();
+            System.out.println(" ValueForServerOnly " + bo.getValueForServerOnly() +" \n");
+            System.out.println(" Obj serialized !");
+            
+        } catch (Exception e) {
+            System.out.println(" IOException " + e.getMessage());
+        }
+        
+        ObjectInputStream objIn = null;        
+        //Deserialization
+        try {
+            fileIn = new FileInputStream(fileNamePath);
+            objIn = new ObjectInputStream(fileIn);
+            
+            Book bookStreamed = (Book)objIn.readObject();
+
+            System.out.println(" ValueForServerOnly " + bookStreamed.getValueForServerOnly()+" \n");
+            System.out.println(" Obj deserialized ! " + bookStreamed);
+            
+            objIn.close();
+            fileIn.close();
+        } catch (Exception e) {
+            System.out.println(" IOException " + e.getMessage());
+        }
+         System.out.println("\n");
+        //============================================== 
     }
 }
+
